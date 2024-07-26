8304ad607406febedbe877852c0995aeeca1e4b3
anishsanghvi7 <anish.sanghvi04@gmail.com>
2024-07-27 01:18:12 +1000

    removing unneeded files

--- workflow/work/69/95e9cd2de58edc245e3b1a3aa79cd2/.command.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -ue
-mkdir -p /srv/scratch/canpang/pangenome_pipeline/results/autoindex_results
-/srv/scratch/canpang/vg autoindex --workflow giraffe -r mock_data.fq -p /srv/scratch/canpang/pangenome_pipeline/results/autoindex_results/index
