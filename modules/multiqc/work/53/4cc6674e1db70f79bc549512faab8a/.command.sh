16e6e65bf774c884a571f67e6d92503bc774075e
anishsanghvi7 <anish.sanghvi04@gmail.com>
2024-08-01 22:15:47 +1000

    fix

--- modules/multiqc/work/53/4cc6674e1db70f79bc549512faab8a/.command.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -ue
-mkdir -p /srv/scratch/canpang/pangenome_pipeline/results/multiqc_report
-multiqc /srv/scratch/canpang/pangenome_pipeline -o /srv/scratch/canpang/pangenome_pipeline/results/multiqc_report
